ac474bb6a556aa767d307d03d27eee10735cea28
jdv-ibm <jdv@zurich.ibm.com>
2020-08-09 13:57:46 +0200

    Docstrings for existing functions.

--- src/wrappers/python/pygroupsig/bldkey.py
+++ src/wrappers/python/pygroupsig/bldkey.py
@@ -3,9 +3,31 @@ from . import constants
 import base64
 
 def bldkey_random(code, grpkey):
-    return lib.groupsig_bld_key_random(code, grpkey)
+    """
+    Generates a random blinding keypair for schemes of the specified code.
+
+    Parameters:
+        code: The code specifying the type of scheme the key belongs to.
+        grpkey: The group key of the scheme. Should contain all parameters
+                needed to generate a random blinding key.
+    Returns:
+        A native data structure containing the blinding keypair. On error,
+        an exception is thrown.
+    """
+    bldkey = lib.groupsig_bld_key_random(code, grpkey)
+    if bldkey == ffi.NULL:
+        raise Exception('Error importing blinding key.')
+    return bldkey
 
 def bldkey_export(bldkey):
+    """
+    Exports the given blinding keypair to a Base64 string.
+    
+    Parameters:
+        bldkey: The blinding key to export.
+    Returns:
+        The produced Base64 string. On error, an Exception is thrown.
+    """
 
     bkey = ffi.new("byte_t **")
     bkey[0] = ffi.NULL
@@ -17,6 +39,14 @@ def bldkey_export(bldkey):
     return b64key
 
 def bldkey_export_pub(bldkey):
+    """
+    Exports the public part of given blinding keypair to a Base64 string.
+    
+    Parameters:
+        bldkey: The blinding key to export.
+    Returns:
+        The produced Base64 string. On error, an Exception is thrown.
+    """    
 
     bkey = ffi.new("byte_t **")
     bkey[0] = ffi.NULL
@@ -28,6 +58,15 @@ def bldkey_export_pub(bldkey):
     return b64key
     
 def bldkey_import(code, b64key):
+    """
+    Imports the given blinding keypair from a Base64 string. Works both for full
+    keypairs and for public keys.
+    
+    Parameters:
+        bldkey: The blinding key to export.
+    Returns:
+        The imported keypair. On error, an Exception is thrown.
+    """    
 
     b = base64.b64decode(b64key)
     bldkey = lib.groupsig_bld_key_import(code, b, len(b))
--- src/wrappers/python/pygroupsig/gml.py
+++ src/wrappers/python/pygroupsig/gml.py
@@ -2,20 +2,55 @@ from _groupsig import lib, ffi
 from . import constants
 
 def gml_init(code):
+    """
+    Initializes a Group Membership List (GML) for schemes of the given type.
+    
+    Parameters:
+        code: The code of the scheme.
+    Returns:
+        A native object representing the GML. Throws an Exception on error.
+    """    
     gml = lib.gml_init(code)
     if gml == ffi.NULL:
         raise Exception('Error initializing GML.')
     return gml
 
 def gml_free(gml):
+    """
+    Frees the native memory used to represent the given GML.
+    
+    Parameters:
+        gml: The GML structure to free.
+    Returns:
+        IOK (1) or IERROR (0)
+    """    
     return lib.gml_free(gml)
 
 def gml_export(gml, filename):
+    """
+    Exports a GML to a file.
+    
+    Parameters:
+        gml: The GML to export.
+        filename: The name of the file to use to store the GML data.
+    Returns:
+        void. Throws an Exception on error.
+    """    
     if lib.gml_export(gml, filename, lib.GML_FILE) == constants.IERROR:
         raise Exception('Error exporting GML to a file.')
     return
 
 def gml_import(code, filename):
+    """
+    Imports a GML from the given file.
+    
+    Parameters:
+        code: The code of the scheme related to this GML.
+        filename: The name of the file to read the GML data from.
+    Returns:
+        The imported GML native data structure. Throws an Exception on error.
+    """    
+    
     gml = lib.gml_import(code, lib.GML_FILE, filename)
     if gml == ffi.NULL:
         raise Exception('Error importing GML.')
