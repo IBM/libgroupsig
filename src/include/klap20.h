ddb4db3f13185ab2d12424498bf6d618cdc0f170
jdv-ibm <jdv@zurich.ibm.com>
2021-04-01 01:39:13 +0200

    Benchmarking tool seems to work up to verify.

--- src/include/klap20.h
+++ src/include/klap20.h
@@ -90,13 +90,13 @@ int klap20_clear();
 
 /** 
  * @fn int klap20_setup(groupsig_key_t *grpkey, 
- *                    groupsig_key_t *mgrkey, 
- *                    gml_t *gml)
+ *                      groupsig_key_t *mgrkey, 
+ *                      gml_t *gml)
  * @brief The setup function for the KLAP20 scheme. Used to generate group public
  *  key and the managers keys.
  * 
  *  In KLAP20, we have two central entities (managers in libgroupsig jargon): the 
- *  Issuer, and the Converter. Both managers have public-private keypairs, their
+ *  Issuer, and the Opener. Both managers have public-private keypairs, their
  *  public parts being a part of the overall group public key. In order to 
  *  properly create the group public key and the manager's keys, we need to call
  *  setup twice. The first time it is called, a partial group public key will be
@@ -104,12 +104,12 @@ int klap20_clear();
  *  to initiate this process.) The second call must receive as input the partial
  *  group public key obtained in the first call, and a new manager key. As a
  *  result of the second call, the group public key is completely set up, and the
- *  Converter's private key is also generated. Therefore, this second call is 
- *  expected to be made by the Converter.
+ *  Opener's private key is also generated. Therefore, this second call is 
+ *  expected to be made by the Opener.
  *
  *  To be precise, whenever an empty group public key (i.e., an initialized KLAP20
  *  groupsig_key_t struct, with all fields in the key sub-struct set to NULL), 
- *  with is assumed, the function assumes that this is a first call.
+ *  is received, the function assumes that this is a first call.
  *
  * @param[in,out] grpkey An initialized group key. In the first call, a partial
  *  group public key will be returned.
@@ -121,8 +121,8 @@ int klap20_clear();
  * @return IOK or IERROR.
  */
 int klap20_setup(groupsig_key_t *grpkey,
-	       groupsig_key_t *mgrkey,
-	       gml_t *gml);
+		 groupsig_key_t *mgrkey,
+		 gml_t *gml);
 
 /**
  * @fn int klap20_get_joinseq(uint8_t *seq)
--- src/tools/benchmarks.c
+++ src/tools/benchmarks.c
@@ -21,6 +21,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <libgen.h>
+#include <openssl/rand.h>
 
 #include "groupsig.h"
 #include "gml.h"
@@ -31,12 +32,99 @@
 #include "message.h"
 #include "misc/misc.h"
 #include "sys/mem.h"
+#include "shim/base64.h"
 #include "misc/profile.h"
 
 log_t logger;
 sysenv_t *sysenv;
 
-int
+static int
+_do_setup
+(
+ int code,
+ groupsig_key_t *grpkey,
+ groupsig_key_t *mgrkey1,
+ groupsig_key_t *mgrkey2,
+ gml_t *gml
+ ) {
+
+  if (!grpkey || !mgrkey1 || !mgrkey2)
+    return IERROR;
+
+  switch(code) {
+  case GROUPSIG_GL19_CODE:
+    if (groupsig_setup(code, grpkey, mgrkey1, gml) == IERROR)
+      return IERROR;
+    if (groupsig_setup(code, grpkey, mgrkey2, gml) == IERROR)
+      return IERROR;    
+    break;
+  case GROUPSIG_KLAP20_CODE:
+    if (groupsig_setup(code, grpkey, mgrkey1, gml) == IERROR)
+      return IERROR;
+    if (groupsig_setup(code, grpkey, mgrkey2, gml) == IERROR)
+      return IERROR;    
+    break;    
+  default:
+    if (groupsig_setup(code, grpkey, mgrkey1, gml) == IERROR)
+      return IERROR;
+    break;
+  }
+  
+}
+
+static message_t*
+_prepare_message()
+{
+
+  byte_t bmsg[500], bscp[500];
+  char *smsg, *sscp, *str;
+  message_t *msg;
+  int msglen, scplen, strl, rc;
+
+  smsg = sscp = str = NULL;
+  msg = NULL;
+  rc = IOK;
+  
+  memset(bmsg, 0, 500);
+  memset(bscp, 0, 500);  
+  
+  msglen = (rand() % 500)+1;
+  scplen = (rand() % 500)+1;  
+  
+  if (!RAND_bytes(bmsg, msglen))
+    GOTOENDRC(IERROR, _prepare_message);
+
+  if (!(smsg = base64_encode(bmsg, msglen, 0)))
+    GOTOENDRC(IERROR, _prepare_message);
+
+  if (!RAND_bytes(bscp, scplen))
+    GOTOENDRC(IERROR, _prepare_message);
+
+  if (!(sscp = base64_encode(bscp, scplen, 0)))
+    GOTOENDRC(IERROR, _prepare_message);  
+
+  strl = strlen(smsg) + strlen(sscp) + strlen("{ \"scope\": \"\", \"message\": \"\" }")+1;
+  if (!(str = mem_malloc(sizeof(char)*strl)))
+    GOTOENDRC(IERROR, _prepare_message);
+
+  sprintf(str, "{ \"scope\": \"%s\", \"message\": \"%s\" }", sscp, smsg);
+  if (!(msg = message_from_string(str)))
+    GOTOENDRC(IERROR, _prepare_message);
+  
+ _prepare_message_end:
+
+  if (smsg) { mem_free(smsg); smsg = NULL; }
+  if (sscp) { mem_free(sscp); sscp = NULL; }
+  
+  if (rc == IERROR) {
+    if (msg) { message_free(msg); msg = NULL; }
+  }
+  
+  return msg;
+  
+}
+
+static int
 _setup_benchmark
 (
  uint8_t code,
@@ -45,7 +133,7 @@ _setup_benchmark
  ) {
   
   const groupsig_t *gh;
-  groupsig_key_t *grpkey, *mgrkey;
+  groupsig_key_t *grpkey, *mgrkey1, *mgrkey2;
   gml_t *gml;
   profile_t *prof;
   struct timeval tv_begin, tv_end;  
@@ -60,7 +148,7 @@ _setup_benchmark
   }
 
   grpkey = NULL;
-  mgrkey = NULL;
+  mgrkey1 = mgrkey2 = NULL;
   gml = NULL;
   prof = NULL;
   rc = IOK;
@@ -77,10 +165,13 @@ _setup_benchmark
   for (i=0; i<iters; i++) {
 
     if (!(grpkey = groupsig_grp_key_init(code)))
-      return IERROR;
-    
-    if (!(mgrkey = groupsig_mgr_key_init(code)))
       GOTOENDRC(IERROR, _setup_benchmark);
+    
+    if (!(mgrkey1 = groupsig_mgr_key_init(code)))
+      GOTOENDRC(IERROR, _setup_benchmark);
+
+    if (!(mgrkey2 = groupsig_mgr_key_init(code)))
+      GOTOENDRC(IERROR, _setup_benchmark);    
     
     if (gh->desc->has_gml)
       if (!(gml = gml_init(code)))
@@ -88,8 +179,8 @@ _setup_benchmark
 
     if (profile_get_time(&tv_begin, &clck_begin, &cycle_begin) == IERROR)
       GOTOENDRC(IERROR, _setup_benchmark);
-    
-    if (groupsig_setup(code, grpkey, mgrkey, gml) == IERROR)
+
+    if (_do_setup(code, grpkey, mgrkey1, mgrkey2, gml) == IERROR)
       GOTOENDRC(IERROR, _setup_benchmark);
     
     if (profile_get_time(&tv_end, &clck_end, &cycle_end) == IERROR)
@@ -109,7 +200,8 @@ _setup_benchmark
  _setup_benchmark_end:
   
   if (grpkey) { groupsig_grp_key_free(grpkey); grpkey = NULL; }
-  if (mgrkey) { groupsig_mgr_key_free(mgrkey); mgrkey = NULL; }
+  if (mgrkey1) { groupsig_mgr_key_free(mgrkey1); mgrkey1 = NULL; }
+  if (mgrkey2) { groupsig_mgr_key_free(mgrkey2); mgrkey2 = NULL; }  
   if (gml && gh->desc->has_gml) { gml_free(gml); gml = NULL; }
   if (prof) { profile_free(prof); prof = NULL; }
   
@@ -117,7 +209,7 @@ _setup_benchmark
   
 }
 
-int
+static int
 _join_benchmark
 (
  uint8_t code,
@@ -126,7 +218,7 @@ _join_benchmark
  ) {
 
   const groupsig_t *gh;
-  groupsig_key_t *grpkey, *mgrkey, *memkey;
+  groupsig_key_t *grpkey, *mgrkey1, *mgrkey2, *memkey;
   message_t *mout, *min;
   gml_t *gml;
   profile_t *prof;
@@ -143,7 +235,8 @@ _join_benchmark
   }
 
   grpkey = NULL;
-  mgrkey = NULL;
+  mgrkey1 = mgrkey2 = NULL;
+  memkey = NULL;
   gml = NULL;
   prof = NULL;
   rc = IOK;
@@ -160,10 +253,13 @@ _join_benchmark
   for (i=0; i<iters; i++) {
 
     if (!(grpkey = groupsig_grp_key_init(code)))
-      return IERROR;
-    
-    if (!(mgrkey = groupsig_mgr_key_init(code)))
       GOTOENDRC(IERROR, _join_benchmark);
+    
+    if (!(mgrkey1 = groupsig_mgr_key_init(code)))
+      GOTOENDRC(IERROR, _join_benchmark);
+
+    if (!(mgrkey2 = groupsig_mgr_key_init(code)))
+      GOTOENDRC(IERROR, _join_benchmark);    
 
     if(!(memkey = groupsig_mem_key_init(code)))
       GOTOENDRC(IERROR, _join_benchmark);
@@ -171,34 +267,29 @@ _join_benchmark
     if (gh->desc->has_gml)
       if (!(gml = gml_init(code)))
 	GOTOENDRC(IERROR, _join_benchmark);
-    
-    if (groupsig_setup(code, grpkey, mgrkey, gml) == IERROR)
+
+    if (_do_setup(code, grpkey, mgrkey1, mgrkey2, gml) == IERROR)
       GOTOENDRC(IERROR, _join_benchmark);
 
-    if (profile_get_time(&tv_begin, &clck_begin, &cycle_begin) == IERROR)
-      GOTOENDRC(IERROR, _join_benchmark);
+    if(groupsig_get_joinstart(code, &start) == IERROR)
+      GOTOENDRC(IERROR, _join_benchmark);      
 
-    //////
-
-    if(groupsig_get_joinstart(code, &start) == IERROR) {
-      return IERROR;
-    }
-
-    if(groupsig_get_joinseq(code, &msgs) == IERROR) {
-      return IERROR;
-    }
+    if(groupsig_get_joinseq(code, &msgs) == IERROR)
+      GOTOENDRC(IERROR, _join_benchmark);      
 
     seq = 0;
     min = NULL; mout = NULL;
 
+    if (profile_get_time(&tv_begin, &clck_begin, &cycle_begin) == IERROR)
+      GOTOENDRC(IERROR, _join_benchmark);
+    
     /* The manager starts */
     if(!start) {
 
       while (seq <= msgs) {
 
-	if(groupsig_join_mgr(&mout, gml, mgrkey, seq, min, grpkey) == IERROR) {
-	  return IERROR;
-	}
+	if(groupsig_join_mgr(&mout, gml, mgrkey1, seq, min, grpkey) == IERROR)
+	  GOTOENDRC(IERROR, _join_benchmark);
 
 	if (min) { message_free(min); min = NULL; }
 	min = mout; mout = NULL;
@@ -206,9 +297,8 @@ _join_benchmark
 
 	if(seq > msgs) break;
 
-	if(groupsig_join_mem(&mout, memkey, seq, min, grpkey) == IERROR) {
-	  return IERROR;
-	}
+	if(groupsig_join_mem(&mout, memkey, seq, min, grpkey) == IERROR)
+	  GOTOENDRC(IERROR, _join_benchmark);
 
 	if (min) { message_free(min); min = NULL; }
 	min = mout; mout = NULL;
@@ -220,19 +310,17 @@ _join_benchmark
 
       while (seq <= msgs) {
 
-	if(groupsig_join_mem(&mout, memkey, seq, min, grpkey) == IERROR) {
-	  return IERROR;
-	}
-
+	if(groupsig_join_mem(&mout, memkey, seq, min, grpkey) == IERROR) 
+	  GOTOENDRC(IERROR, _join_benchmark);
+	
 	if (min) { message_free(min); min = NULL; }
 	min = mout; mout = NULL;
 	seq++;
 	
 	if(seq > msgs) break;
 	
-	if(groupsig_join_mgr(&mout, gml, mgrkey, seq, min, grpkey) == IERROR) {
-	  return IERROR;
-	}
+	if(groupsig_join_mgr(&mout, gml, mgrkey1, seq, min, grpkey) == IERROR)
+	  GOTOENDRC(IERROR, _join_benchmark);
 
 	if (min) { message_free(min); min = NULL; }
 	min = mout; mout = NULL;
@@ -241,16 +329,14 @@ _join_benchmark
       }      
       
     }
+    
+    if (profile_get_time(&tv_end, &clck_end, &cycle_end) == IERROR)
+      GOTOENDRC(IERROR, _join_benchmark);    
 
     if (memkey) { groupsig_mem_key_free(memkey); memkey = NULL; }
     if (min) { message_free(min); min = NULL; }
     if (mout) { message_free(mout); mout = NULL; }    
-    
-    //////
-    
-    if (profile_get_time(&tv_end, &clck_end, &cycle_end) == IERROR)
-      GOTOENDRC(IERROR, _join_benchmark);
-    
+            
     if (profile_add_entry(prof,
 			  &tv_begin, &tv_end,
 			  clck_begin, clck_end,
@@ -265,7 +351,337 @@ _join_benchmark
  _join_benchmark_end:
   
   if (grpkey) { groupsig_grp_key_free(grpkey); grpkey = NULL; }
-  if (mgrkey) { groupsig_mgr_key_free(mgrkey); mgrkey = NULL; }
+  if (mgrkey1) { groupsig_mgr_key_free(mgrkey1); mgrkey1 = NULL; }
+  if (mgrkey2) { groupsig_mgr_key_free(mgrkey2); mgrkey2 = NULL; }  
+  if (memkey) { groupsig_mem_key_free(memkey); memkey = NULL; }
+  if (gml && gh->desc->has_gml) { gml_free(gml); gml = NULL; }
+  if (prof) { profile_free(prof); prof = NULL; }
+  
+  return rc;
+  
+}
+
+static int
+_sign_benchmark
+(
+ uint8_t code,
+ char *filename,
+ uint32_t iters
+ ) {
+
+  const groupsig_t *gh;
+  groupsig_key_t *grpkey, *mgrkey1, *mgrkey2, *memkey;
+  groupsig_signature_t *sig;
+  message_t *mout, *min, *msg;
+  gml_t *gml;
+  profile_t *prof;
+  struct timeval tv_begin, tv_end;  
+  clock_t clck_begin, clck_end;
+  uint64_t cycle_begin, cycle_end;  
+  uint32_t i;
+  uint8_t start, seq, msgs;
+  int rc;
+
+  if (!filename || !iters) {
+    fprintf(stderr, "_sign_benchmark: wrong parameters.\n");
+    return IERROR;
+  }
+
+  grpkey = NULL;
+  mgrkey1 = mgrkey2 = NULL;
+  gml = NULL;
+  prof = NULL;
+  rc = IOK;
+  
+  if (!(gh = groupsig_get_groupsig_from_code(code))) {
+    return IERROR;
+  }
+  
+  prof = profile_begin(filename);
+  if (!prof) {
+    return IERROR;
+  }
+
+  for (i=0; i<iters; i++) {
+
+    if (!(grpkey = groupsig_grp_key_init(code)))
+      GOTOENDRC(IERROR, _sign_benchmark);
+    
+    if (!(mgrkey1 = groupsig_mgr_key_init(code)))
+      GOTOENDRC(IERROR, _sign_benchmark);
+
+    if (!(mgrkey2 = groupsig_mgr_key_init(code)))
+      GOTOENDRC(IERROR, _sign_benchmark);    
+
+    if(!(memkey = groupsig_mem_key_init(code)))
+      GOTOENDRC(IERROR, _sign_benchmark);
+    
+    if (gh->desc->has_gml)
+      if (!(gml = gml_init(code)))
+	GOTOENDRC(IERROR, _sign_benchmark);
+
+    if (_do_setup(code, grpkey, mgrkey1, mgrkey2, gml) == IERROR)
+      GOTOENDRC(IERROR, _sign_benchmark);
+
+    if(groupsig_get_joinstart(code, &start) == IERROR)
+      GOTOENDRC(IERROR, _sign_benchmark);      
+
+    if(groupsig_get_joinseq(code, &msgs) == IERROR)
+      GOTOENDRC(IERROR, _sign_benchmark);      
+
+    seq = 0;
+    min = NULL; mout = NULL;
+    
+    /* The manager starts */
+    if(!start) {
+
+      while (seq <= msgs) {
+
+	if(groupsig_join_mgr(&mout, gml, mgrkey1, seq, min, grpkey) == IERROR)
+	  GOTOENDRC(IERROR, _sign_benchmark);
+
+	if (min) { message_free(min); min = NULL; }
+	min = mout; mout = NULL;
+	seq++;
+
+	if(seq > msgs) break;
+
+	if(groupsig_join_mem(&mout, memkey, seq, min, grpkey) == IERROR)
+	  GOTOENDRC(IERROR, _sign_benchmark);
+
+	if (min) { message_free(min); min = NULL; }
+	min = mout; mout = NULL;
+	seq++;
+
+      }
+ 
+    } else { /* The member starts */
+
+      while (seq <= msgs) {
+
+	if(groupsig_join_mem(&mout, memkey, seq, min, grpkey) == IERROR)
+	  GOTOENDRC(IERROR, _sign_benchmark);
+
+	if (min) { message_free(min); min = NULL; }
+	min = mout; mout = NULL;
+	seq++;
+	
+	if(seq > msgs) break;
+	
+	if(groupsig_join_mgr(&mout, gml, mgrkey1, seq, min, grpkey) == IERROR)
+	  GOTOENDRC(IERROR, _sign_benchmark);
+
+	if (min) { message_free(min); min = NULL; }
+	min = mout; mout = NULL;
+	seq++;
+
+      }      
+      
+    }
+
+    if (!(msg = _prepare_message()))
+      GOTOENDRC(IERROR, _sign_benchmark);
+
+    if(!(sig = groupsig_signature_init(code)))
+      GOTOENDRC(IERROR, _sign_benchmark);
+
+    if (profile_get_time(&tv_begin, &clck_begin, &cycle_begin) == IERROR)
+      GOTOENDRC(IERROR, _sign_benchmark);    
+
+    if(groupsig_sign(sig, msg, memkey, grpkey, UINT_MAX) == IERROR)
+      GOTOENDRC(IERROR, _sign_benchmark);
+
+    if (profile_get_time(&tv_end, &clck_end, &cycle_end) == IERROR)
+      GOTOENDRC(IERROR, _sign_benchmark);    
+
+    if (sig) { groupsig_signature_free(sig); sig = NULL; }
+    if (memkey) { groupsig_mem_key_free(memkey); memkey = NULL; }
+    if (min) { message_free(min); min = NULL; }
+    if (mout) { message_free(mout); mout = NULL; }
+    if (msg) { message_free(msg); msg = NULL; }    
+            
+    if (profile_add_entry(prof,
+			  &tv_begin, &tv_end,
+			  clck_begin, clck_end,
+			  cycle_begin, cycle_end) == IERROR)
+      GOTOENDRC(IERROR, _sign_benchmark);
+
+  }
+
+  if (profile_process_and_dump(prof, code, "sign") == IERROR)
+    GOTOENDRC(IERROR, _sign_benchmark);
+    
+ _sign_benchmark_end:
+  
+  if (grpkey) { groupsig_grp_key_free(grpkey); grpkey = NULL; }
+  if (mgrkey1) { groupsig_mgr_key_free(mgrkey1); mgrkey1 = NULL; }
+  if (mgrkey2) { groupsig_mgr_key_free(mgrkey2); mgrkey2 = NULL; }  
+  if (memkey) { groupsig_mem_key_free(memkey); memkey = NULL; }
+  if (gml && gh->desc->has_gml) { gml_free(gml); gml = NULL; }
+  if (prof) { profile_free(prof); prof = NULL; }
+  
+  return rc;
+  
+}
+
+static int
+_verify_benchmark
+(
+ uint8_t code,
+ char *filename,
+ uint32_t iters
+ ) {
+
+  const groupsig_t *gh;
+  groupsig_key_t *grpkey, *mgrkey1, *mgrkey2, *memkey;
+  groupsig_signature_t *sig;
+  message_t *mout, *min, *msg;
+  gml_t *gml;
+  profile_t *prof;
+  struct timeval tv_begin, tv_end;  
+  clock_t clck_begin, clck_end;
+  uint64_t cycle_begin, cycle_end;  
+  uint32_t i;
+  uint8_t start, seq, msgs, b;
+  int rc;
+
+  if (!filename || !iters) {
+    fprintf(stderr, "_verify_benchmark: wrong parameters.\n");
+    return IERROR;
+  }
+
+  grpkey = NULL;
+  mgrkey1 = mgrkey2 = NULL;
+  gml = NULL;
+  prof = NULL;
+  rc = IOK;
+  
+  if (!(gh = groupsig_get_groupsig_from_code(code))) {
+    return IERROR;
+  }
+  
+  prof = profile_begin(filename);
+  if (!prof) {
+    return IERROR;
+  }
+
+  for (i=0; i<iters; i++) {
+
+    if (!(grpkey = groupsig_grp_key_init(code)))
+      GOTOENDRC(IERROR, _verify_benchmark);
+    
+    if (!(mgrkey1 = groupsig_mgr_key_init(code)))
+      GOTOENDRC(IERROR, _verify_benchmark);
+
+    if (!(mgrkey2 = groupsig_mgr_key_init(code)))
+      GOTOENDRC(IERROR, _verify_benchmark);    
+
+    if(!(memkey = groupsig_mem_key_init(code)))
+      GOTOENDRC(IERROR, _verify_benchmark);
+    
+    if (gh->desc->has_gml)
+      if (!(gml = gml_init(code)))
+	GOTOENDRC(IERROR, _verify_benchmark);
+
+    if (_do_setup(code, grpkey, mgrkey1, mgrkey2, gml) == IERROR)
+      GOTOENDRC(IERROR, _verify_benchmark);
+
+    if(groupsig_get_joinstart(code, &start) == IERROR)
+      GOTOENDRC(IERROR, _verify_benchmark);      
+
+    if(groupsig_get_joinseq(code, &msgs) == IERROR)
+      GOTOENDRC(IERROR, _verify_benchmark);      
+
+    seq = 0;
+    min = NULL; mout = NULL;
+    
+    /* The manager starts */
+    if(!start) {
+
+      while (seq <= msgs) {
+
+	if(groupsig_join_mgr(&mout, gml, mgrkey1, seq, min, grpkey) == IERROR)
+	  GOTOENDRC(IERROR, _verify_benchmark);
+	
+	if (min) { message_free(min); min = NULL; }
+	min = mout; mout = NULL;
+	seq++;
+
+	if(seq > msgs) break;
+
+	if(groupsig_join_mem(&mout, memkey, seq, min, grpkey) == IERROR)
+	  GOTOENDRC(IERROR, _verify_benchmark);
+	
+	if (min) { message_free(min); min = NULL; }
+	min = mout; mout = NULL;
+	seq++;
+
+      }
+ 
+    } else { /* The member starts */
+
+      while (seq <= msgs) {
+
+	if(groupsig_join_mem(&mout, memkey, seq, min, grpkey) == IERROR)
+	  GOTOENDRC(IERROR, _verify_benchmark);
+	
+	if (min) { message_free(min); min = NULL; }
+	min = mout; mout = NULL;
+	seq++;
+	
+	if(seq > msgs) break;
+	
+	if(groupsig_join_mgr(&mout, gml, mgrkey1, seq, min, grpkey) == IERROR)
+	  GOTOENDRC(IERROR, _verify_benchmark);
+	
+	if (min) { message_free(min); min = NULL; }
+	min = mout; mout = NULL;
+	seq++;
+
+      }      
+      
+    }
+
+    if (!(msg = _prepare_message()))
+      GOTOENDRC(IERROR, _verify_benchmark);
+
+    if(!(sig = groupsig_signature_init(code)))
+      GOTOENDRC(IERROR, _verify_benchmark);
+
+    if(groupsig_sign(sig, msg, memkey, grpkey, UINT_MAX) == IERROR)
+      GOTOENDRC(IERROR, _verify_benchmark);
+
+    if (profile_get_time(&tv_begin, &clck_begin, &cycle_begin) == IERROR)
+      GOTOENDRC(IERROR, _verify_benchmark);    
+
+    if (groupsig_verify(&b, sig, msg, grpkey) == IERROR)
+      GOTOENDRC(IERROR, _verify_benchmark);
+
+    if (profile_get_time(&tv_end, &clck_end, &cycle_end) == IERROR)
+      GOTOENDRC(IERROR, _verify_benchmark);    
+
+    if (sig) { groupsig_signature_free(sig); sig = NULL; }
+    if (memkey) { groupsig_mem_key_free(memkey); memkey = NULL; }
+    if (min) { message_free(min); min = NULL; }
+    if (mout) { message_free(mout); mout = NULL; }
+    if (msg) { message_free(msg); msg = NULL; }    
+            
+    if (profile_add_entry(prof,
+			  &tv_begin, &tv_end,
+			  clck_begin, clck_end,
+			  cycle_begin, cycle_end) == IERROR)
+      GOTOENDRC(IERROR, _verify_benchmark);
+
+  }
+
+  if (profile_process_and_dump(prof, code, "verify") == IERROR)
+    GOTOENDRC(IERROR, _verify_benchmark);
+    
+ _verify_benchmark_end:
+  
+  if (grpkey) { groupsig_grp_key_free(grpkey); grpkey = NULL; }
+  if (mgrkey1) { groupsig_mgr_key_free(mgrkey1); mgrkey1 = NULL; }
+  if (mgrkey2) { groupsig_mgr_key_free(mgrkey2); mgrkey2 = NULL; }  
   if (memkey) { groupsig_mem_key_free(memkey); memkey = NULL; }
   if (gml && gh->desc->has_gml) { gml_free(gml); gml = NULL; }
   if (prof) { profile_free(prof); prof = NULL; }
@@ -342,17 +758,17 @@ int main(int argc, char *argv[]) {
 	return IERROR;
       }
 
-      /* if (_sign_benchmark(code, filename, iters) == IERROR) { */
-      /* 	fprintf(stderr, "Error benchmarking sign for scheme with code %u\n", */
-      /* 		code); */
-      /* 	return IERROR; */
-      /* } */
+      if (_sign_benchmark(code, filename, iters) == IERROR) {
+	fprintf(stderr, "Error benchmarking sign for scheme with code %u\n",
+		code);
+	return IERROR;
+      }
 
-      /* if (_verify_benchmark(code, filename, iters) == IERROR) { */
-      /* 	fprintf(stderr, "Error benchmarking verify for scheme with code %u\n", */
-      /* 		code); */
-      /* 	return IERROR; */
-      /* } */
+      if (_verify_benchmark(code, filename, iters) == IERROR) {
+	fprintf(stderr, "Error benchmarking verify for scheme with code %u\n",
+		code);
+	return IERROR;
+      }
 
       /* if (gh->verify_batch) { */
       /* 	if (_verify_batch_benchmark(code, filename, iters) == IERROR) { */
