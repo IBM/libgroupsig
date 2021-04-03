d7f7c48fb19d3a4dd87d74a13cbd5e43b7621797
jdv-ibm <jdv@zurich.ibm.com>
2021-04-04 00:00:59 +0200

    Fixing copy-paste licensing error.

--- src/groupsig/dl21seq/mem_key.h
+++ src/groupsig/dl21seq/mem_key.h
@@ -1,37 +1,20 @@
-/*                               -*- Mode: C -*- 
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *	libgroupsig Group Signatures library
- *	Copyright (C) 2012-2013 Jesus Diaz Vico
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- *		
- *
- *	This file is part of the libgroupsig Group Signatures library.
- *
- *
- *  The libgroupsig library is free software: you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License as 
- *  defined by the Free Software Foundation, either version 3 of the License, 
- *  or any later version.
- *
- *  The libroupsig library is distributed WITHOUT ANY WARRANTY; without even 
- *  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
- *  See the GNU Lesser General Public License for more details.
- *
- *
- *  You should have received a copy of the GNU Lesser General Public License 
- *  along with Group Signature Crypto Library.  If not, see <http://www.gnu.org/
- *  licenses/>
- *
- * @file: mem_key.h
- * @brief: DL21SEQ member keys.
- * @author: jesus
- * Maintainer: jesus
- * @date: mié may  9 17:11:58 2012 (+0200)
- * @version: 0.1
- * Last-Updated: vie ago 23 11:00:59 2013 (+0200)
- *           By: jesus
- *     Update #: 5
- * URL: bitbucket.org/jdiazvico/libgroupsig
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 #ifndef _DL21SEQ_MEM_KEY_H
--- src/include/dl21seq.h
+++ src/include/dl21seq.h
@@ -1,40 +1,20 @@
-/*                               -*- Mode: C -*- 
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *	libgroupsig Group Signatures library
- *	Copyright (C) 2012-2013 Jesus Diaz Vico
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- *		
- *
- *	This file is part of the libgroupsig Group Signatures library.
- *
- *
- *  The libgroupsig library is free software: you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License as 
- *  defined by the Free Software Foundation, either version 3 of the License, 
- *  or any later version.
- *
- *  The libroupsig library is distributed WITHOUT ANY WARRANTY; without even 
- *  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
- *  See the GNU Lesser General Public License for more details.
- *
- *
- *  You should have received a copy of the GNU Lesser General Public License 
- *  along with Group Signature Crypto Library.  If not, see <http://www.gnu.org/
- *  licenses/>
- *
- * @file: dl21seq.h
- * @brief: Main definitions for the DL21SEQ group signature scheme.
- *
- * The DL21SEQ scheme is an implementation of the scheme XXX
- *
- * @author: jesus
- * Maintainer: jesus
- * @date: vie jul  6 11:59:40 2012 (+0200)
- * @version: 0.1
- * Last-Updated: lun ago  5 15:31:12 2013 (+0200)
- *           By: jesus
- *     Update #: 129
- * URL: bitbucket.org/jdiazvico/libgroupsig
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 #ifndef _DL21SEQ_H
